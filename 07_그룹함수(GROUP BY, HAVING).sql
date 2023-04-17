3e8bf91838a60a15fb6fbfba9a7cae4aa64e486a
Jane <ribbon0628@naver.com>
2023-04-17 18:14:58 +0900

    23.04.17

--- 07_그룹함수(GROUP BY, HAVING).sql
+++ 07_그룹함수(GROUP BY, HAVING).sql
@@ -47,3 +47,101 @@ SELECT
 FROM employees
 GROUP BY department_id
 HAVING SUM(salary) > 100000;
+
+
+/*
+    문제 1.
+    사원 테이블에서 JOB_ID별 사원 수를 구하세요.
+    사원 테이블에서 JOB_ID별 월급의 평균을 구하세요. 월급의 평균 순으로 내림차순 정렬하세요
+*/
+SELECT
+    job_id,
+    COUNT(job_id) AS 사원수,
+    AVG(salary) AS 평균월급
+FROM employees
+GROUP BY job_id
+ORDER BY 평균월급 DESC;
+
+-----------------------------------
+
+SELECT
+    job_id,
+    COUNT(*) AS 사원수
+FROM employees
+GROUP BY job_id;
+
+SELECT
+    job_id,
+    AVG(salary) AS 평균월급
+FROM employees
+GROUP BY job_id
+ORDER BY 평균월급 DESC;
+
+/*
+    문제 2.
+    사원 테이블에서 입사 년도 별 사원 수를 구하세요.
+    (TO_CHAR() 함수를 사용해서 연도만 반환합니다. 그리고 그것을 그룹화합니다.)
+*/
+SELECT
+    COUNT(TO_CHAR(hire_date, 'YYYY'))
+FROM employees
+GROUP BY TO_CHAR(hire_date, 'YYYY');
+
+-----------------------------------
+
+SELECT
+    TO_CHAR(hire_date, 'YY'),
+    COUNT(*)
+FROM employees
+GROUP BY TO_CHAR(hire_date, 'YY');
+
+/*
+    문제 3.
+    급여가 5000 이상인 사원들의 부서별 평균 급여를 출력하세요.
+    단 부서 평균 급여가 7000이상인 부서만 출력하세요.
+*/
+SELECT
+    department_id,
+    AVG(salary)
+FROM employees
+WHERE salary >= 5000
+GROUP BY department_id
+HAVING AVG(salary) >= 7000;
+
+-----------------------------------
+
+SELECT
+    department_id,
+    AVG(salary) AS 평균급여
+FROM employees
+WHERE salary >= 5000
+GROUP BY department_id
+HAVING AVG(salary) >= 7000;
+
+/*
+    문제 4.
+    사원 테이블에서 commission_pct(커미션) 컬럼이 null이 아닌 사람들의
+    department_id(부서별) salary(월급)의 평균, 합계, count를 구합니다.
+    조건 1) 월급의 평균은 커미션을 적용시킨 월급입니다.
+    조건 2) 평균은 소수 2째 자리에서 절삭 하세요
+*/
+SELECT * FROM employees;
+SELECT
+    NVL(department_id, 0) AS 부서,
+    TO_CHAR(TRUNC(AVG(salary + salary*commission_pct), 2), '$9,999,999.99') AS 평균월급,
+    TO_CHAR(SUM(salary + salary*commission_pct), '$9,999,999') AS 월급합계,
+    COUNT(salary) AS 직원수
+FROM employees
+WHERE commission_pct IS NOT NULL
+GROUP BY department_id;
+
+-----------------------------------
+
+SELECT
+    department_id,
+    TRUNC(AVG(salary + salary * commission_pct), 2),
+    SUM(salary + salary*commission_pct),
+    COUNT(*)
+FROM employees
+WHERE commission_pct IS NOT NULL
+GROUP BY department_id;
